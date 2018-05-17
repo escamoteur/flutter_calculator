93a216c45018702a8ead4490081d7edae59a76e1
escamoteur <github@burkharts.net>
2018-05-17 13:42:09 +0200

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -33,49 +35,53 @@ class MyAppState extends State<MyApp> {
       }
       queuedOperation = operation;
       operand = 0.0;
-      setState(() => resultString = accu.toString());
+      var result = accu.toString();
+      setState(() => resultString = result.toString().substring(0, min(10,result.length) ));
   }
 
-  List<Widget>  buildNumberButtons(int from, int to, int flex, {int count = 3 }) {
-    return new Iterable.generate(count,(index)
+  List<Widget>  buildNumberButtons( int count,int from, int flex) {
+    return new Iterable.generate(count, (index)
                   {
                       return new Expanded(flex: flex,
                               child: new Padding(padding: const EdgeInsets.all(1.0),
                                             child: FlatButton(onPressed: () => numberPressed(from + index), color: Colors.white,
-                          child: Text("${from + index}", style: TextStyle(fontSize: 40.0),)),
+                                                  child: Text("${from + index}", style: TextStyle(fontSize: 40.0),)),
                               ),
                       );
                   }).toList();
   }
 
-
-
   Widget buildOperatorButton(String label, OperatorFunc func, int flex, {Color color = Colors.amber})
   {
       return Expanded(flex: flex,
               child: Padding(
-                        padding: const EdgeInsets.all(1.0),
-                        child: FlatButton(onPressed: () => calc(func), color: color, 
-                            child: Text(label, style: TextStyle(fontSize: 40.0),)),
+                  padding: const EdgeInsets.all(1.0),
+                  child: FlatButton(onPressed: () => calc(func), color: color, 
+                      child: Text(label, style: TextStyle(fontSize: 40.0),)),
                       ),
       );
   }
 
-
+  Widget buildRow(int numberKeyCount, int startNumber,  int numberFlex, String operationLabel, OperatorFunc operation, int operrationFlex)
+  {
+      return new Expanded(child: 
+                Row(crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: new List.from(buildNumberButtons(numberKeyCount,startNumber ,numberFlex,))
+                                    ..add(buildOperatorButton(operationLabel, operation, operrationFlex))));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      
+    return new MaterialApp(      
       home: new SafeArea(
               child: new Material(color: Colors.black,
                 child: Column( crossAxisAlignment: CrossAxisAlignment.end,
                    children: <Widget>[
             Expanded(child: Row(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.end, children: [Text(resultString,textAlign: TextAlign.right,  style: TextStyle(fontSize: 50.0, color: Colors.white),)])),
-            Expanded(child: Row(crossAxisAlignment: CrossAxisAlignment.stretch,children: new List.from(buildNumberButtons(7, 9,1))..add(buildOperatorButton("/", (accu, dividor)=> accu / dividor , 1)))),
-            Expanded(child:Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: new List.from(buildNumberButtons(4, 6,1))..add(buildOperatorButton("x", (accu, dividor)=> accu * dividor , 1)))),
-            Expanded(child:Row(crossAxisAlignment: CrossAxisAlignment.stretch,children: new List.from(buildNumberButtons(1, 3,1))..add(buildOperatorButton("-", (accu, dividor)=> accu - dividor , 1)))),
-            Expanded(child:Row(crossAxisAlignment: CrossAxisAlignment.stretch,children: new List.from(buildNumberButtons(0, 0,3, count: 1))..add(buildOperatorButton("+", (accu, dividor)=> accu + dividor , 1)))),
+            buildRow(3,7,1,"/", (accu, dividor)=> accu / dividor , 1),
+            buildRow(3,4,1,"x", (accu, dividor)=> accu * dividor , 1),
+            buildRow(3,1,1,"-", (accu, dividor)=> accu - dividor , 1),
+            buildRow(1, 0,3,"+", (accu, dividor)=> accu + dividor , 1),
             Expanded(child:Row(crossAxisAlignment: CrossAxisAlignment.stretch,children: [buildOperatorButton("C", null, 1, color: Colors.grey),buildOperatorButton("=", (accu, dividor)=> accu, 3)],))
           ],),
         ),
